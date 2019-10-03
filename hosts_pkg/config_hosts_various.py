5e647e23cc2271f4190cfab9bde0cb17a39b1115
Paweł Owczarek <pawel@ow.cx>
2019-10-03 08:53:26 +0200

    Comment out unavailable host file

--- hosts_pkg/config_hosts_various.py
+++ hosts_pkg/config_hosts_various.py
@@ -1,5 +1,5 @@
 URL_HOST_FILES = [
-    "https://adaway.org/hosts.txt",
+    # "https://adaway.org/hosts.txt",
     "http://winhelp2002.mvps.org/hosts.txt",
     "http://hosts-file.net/ad_servers.txt",
     "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
